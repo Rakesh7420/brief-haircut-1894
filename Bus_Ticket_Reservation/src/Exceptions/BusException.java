e216e0a8bf7cbfdfbd38ca570969571dbfdc891a
Rakesh Gudur <rakeshgudur7420@gmail.com>
2022-11-12 00:18:57 +0530

    day3

---
+++ Bus_Ticket_Reservation/src/Exceptions/BusException.java
@@ -0,0 +1,14 @@
+package Exceptions;
+
+public class BusException extends Exception {
+
+	public BusException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	public BusException(String msg) {
+		super(msg);
+	}
+
+}
